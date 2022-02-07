a67159391325c90d1e41be5bc97da90a6fca6f65
citytexi <heekyne66@gmail.com>
2022-02-07 12:38:18 +0900

    bronze5 modify

--- src/bronze/bronze_5/B_1000.kt
+++ src/bronze/bronze_5/B_1000.kt
@@ -2,6 +2,17 @@ package bronze.bronze_5
 
 import java.util.*
 
+/**
+ * @문제
+ * 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+ *
+ * @입력
+ * 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+ *
+ * @출력
+ * 첫째 줄에 A+B를 출력한다.
+**/
+
 class B_1000 {
     fun solution(){
         val sc = Scanner(System.`in`)
--- src/bronze/bronze_5/B_2557.kt
+++ src/bronze/bronze_5/B_2557.kt
@@ -1,5 +1,16 @@
 package bronze.bronze_5
 
+/**
+ * @문제
+ * Hello World!를 출력하시오.
+ *
+ * @입력
+ * 없음
+ *
+ * @출력
+ * Hello World!를 출력하시오.
+ **/
+
 class B_2557{
     fun solution(){
         println("Hello World!")
